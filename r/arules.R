88daa6f90950a0fa27bb7c2445f20311e96b8550
Hoang Long <litpuvn@gmail.com>
2017-10-11 11:22:44 -0500

    update arules

--- r/arules.R
+++ r/arules.R
@@ -7,13 +7,7 @@ tr <- read.transactions("data/transactions.csv", format = "basket", sep=",", ski
 
 frequentItems <- eclat (tr, parameter = list(supp = 0.05, maxlen = 50)) # calculates support for frequent items
 inspect(frequentItems)
-#>    items                         support   
-#> 1  {other vegetables,whole milk} 0.07483477
-#> 2  {whole milk}                  0.25551601
-#> 3  {other vegetables}            0.19349263
-#> 4  {rolls/buns}                  0.18393493
-#> 5  {yogurt}                      0.13950178
-#> 6  {soda}                        0.17437722
+
 itemFrequencyPlot(tr, topN=15, type="absolute", main="Item Frequency") # plot frequent items
 
 
