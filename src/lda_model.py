17c5e7de5dddacdd83619ea10c49f2271c0de8a4
Hoang Long <litpuvn@gmail.com>
2018-02-24 16:06:19 -0600

    add lda vis

--- src/lda_model.py
+++ src/lda_model.py
@@ -12,10 +12,9 @@ import matplotlib.pyplot as plt
 import pyLDAvis.gensim
 
 
-
 def create_lda_model(corpus_file, dict_file, total_topics=5):
     dictionary = corpora.Dictionary.load(dict_file)
-    corpus = corpora.MmCorpus.load(corpus_file)
+    corpus = corpora.MmCorpus(corpus_file)
 
     tfidf = models.TfidfModel(corpus) # step 1 -- initialize a model
     corpus_tfidf = tfidf[corpus]  # step 2 -- use the model to transform vectors
@@ -26,15 +25,17 @@ def create_lda_model(corpus_file, dict_file, total_topics=5):
     return lda, corpus_lda, dictionary, total_topics
 
 
-
-mm_corpus_file = 'output/data.mm'
-mm_dict_file = 'output/data.dict'
+fileName = 'elon'
+mm_corpus_file = 'output/' + fileName + '.mm'
+mm_dict_file = 'output/' + fileName + '.dict'
 NUM_WORDS = 5
 
-lda, corpus_lda, dictionary, total_topics = create_lda_model(corpus_file=mm_dict_file, dict_file=mm_dict_file)
+lda, corpus_lda, dictionary, total_topics = create_lda_model(corpus_file=mm_corpus_file, dict_file=mm_dict_file)
 
 # Show first n important word in the topics:
-lda.show_topics(total_topics, num_words=NUM_WORDS)
+topic_words = lda.show_topics(total_topics, num_words=NUM_WORDS)
+
+print(topic_words)
 
 data_lda = {i: OrderedDict(lda.show_topic(i, 25)) for i in range(total_topics)}
 df_lda = pd.DataFrame(data_lda)
@@ -45,5 +46,11 @@ plt.setp(g.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
 plt.show()
 #plt.setp(ax_heatmap.get_yticklabels(), rotation=0)  # For y axis
 
-pyLDAvis.enable_notebook()
-panel = pyLDAvis.gensim.prepare(lda, corpus_lda, dictionary, mds='tsne')
+# pyLDAvis.s
+# pyLDAvis.enable_notebook()
+# panel = pyLDAvis.gensim.prepare(lda, corpus_lda, dictionary, mds='tsne')
+
+# prepare LDA_topics model visualization
+LDA_vis_data = pyLDAvis.gensim.prepare(lda, corpus_lda, dictionary)
+# save visualization
+pyLDAvis.save_html(LDA_vis_data, 'output/vis/topic.html')
