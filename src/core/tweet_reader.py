3c9a1f1ec91b35632d83ba8d23ea06bc082bfa1d
Hoang Long <litpuvn@gmail.com>
2018-02-25 15:08:20 -0600

    add topwords

---
+++ src/core/tweet_reader.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import re
+from core.stop_word_builder import StopWordBuilder
+
+
+class TweetReader:
+    def __init__(self, tweet_file, text_column='tweet', separator=',', encoding='latin1'):
+        self.tweets_df = pd.read_csv(tweet_file, sep=separator, encoding=encoding)
+        all_tweets = self.tweets_df[text_column]
+
+        self.corpus = []
+        for tweet in all_tweets:
+            self.corpus.append(tweet)
+
+    @staticmethod
+    def _clean_text_data(text_array, stop_words):
+        stop_builder = StopWordBuilder(stop_words)
+
+        stop_list = stop_builder.get_stop_words()
+
+        texts = [[word for word in re.findall('[a-z]{3,15}', str(document).lower()) if word not in stop_list] for document in text_array]
+
+        return texts
+
+    def get_corpus(self):
+        return self.corpus
+
+    def get_tweets_df(self):
+        return self.tweets_df
+
+    def get_total_tweets(self):
+        return len(self.get_corpus())
+
+    def extract_words_frequency(self, num_words=None, stop_word_file=''):
+        my_clean_tweets = []
+
+        if len(stop_word_file) > 0:
+            my_clean_tweets = TweetReader._clean_text_data(self.corpus, stop_word_file)
+
+        freq = {}
+
+        for tweet in my_clean_tweets:
+            for word in tweet:
+                count = freq.get(word, 0)
+                freq[word] = count + 1
+
+        frequency_list = freq.keys()
+        results = []
+        for word in frequency_list:
+            tuple = (word, freq[word])
+            results.append(tuple)
+
+        byFreq = sorted(results, key=lambda word: word[1], reverse=True)
+
+        if num_words is not None:
+            return byFreq[: num_words]
+
+        return byFreq
--- src/top_words_frequency.py
+++ src/top_words_frequency.py
@@ -1,56 +1,20 @@
 from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import re
-import string
+from core.tweet_reader import TweetReader
 
-
-# # Read CSV file, get author names and counts.
-# df = pd.read_csv("books.csv", index_col="id")
-# counter = Counter(df['author'])
-# author_names = counter.keys()
-# author_counts = counter.values()
-#
-# # Plot histogram using matplotlib bar().
-# indexes = np.arange(len(author_names))
-# width = 0.7
-# plt.bar(indexes, author_counts, width)
-# plt.xticks(indexes + width * 0.5, author_names)
-# plt.show()
-
-
-
-
-freq={}
-read_text=open('WordCount.txt','r')
-final_text=read_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{3,15}\b', final_text)
-match_pattern[:3]
-
-for word in match_pattern:
-    count = freq.get(word, 0)
-    freq[word]=count + 1
-
-frequency_list = freq.keys()
-
-results = []
-for word in frequency_list:
-    tuple = (word, freq[word])
-    results.append(tuple)
-
-byFreq = sorted(results, key=lambda word: word[1], reverse=True)
+reader = TweetReader('data/pos_neg/neg_comment.csv',  text_column='text', separator='\001', encoding='utf8')
+freq = reader.extract_words_frequency(num_words=10, stop_word_file='input/stopwords.txt')
 
 words_names=[]
 words_count=[]
-for (word, freq) in byFreq[:10]:
-    print (word, freq)
+for (word, freq) in freq:
     words_names.append(word)
     words_count.append(freq)
 
 
 # Plot histogram using matplotlib bar()
-plt.xlabel('Top 10 Words')
+plt.xlabel('Top Words')
 plt.ylabel('Frequency')
 plt.title('Plotting Word Frequency')
 indexes = np.arange(len(words_names) )
