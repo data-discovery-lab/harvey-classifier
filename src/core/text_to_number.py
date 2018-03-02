776eb4844824c0e6a8b279bbca8528f2a5e03e79
Hoang Long <litpuvn@gmail.com>
2018-03-02 14:46:30 -0600

    add util to convert text to number

---
+++ src/core/text_to_number.py
@@ -0,0 +1,21 @@
+from ktext.preprocess import processor
+import dill as dpickle
+import numpy as np
+
+
+class TextToNumber:
+
+    @staticmethod
+    def create_number_vector(raw_text_array, max_uniqie_words_size=8000, padding_maxlen=70, padding_position='post',
+                             output_file='nummerical_text'):
+        body_pp = processor(keep_n=max_uniqie_words_size, padding_maxlen=padding_maxlen, padding=padding_position)
+        vecs = body_pp.fit_transform(raw_text_array)
+
+        # Save the preprocessor
+        with open(output_file + '.dpkl', 'wb') as f:
+            dpickle.dump(body_pp, f)
+
+        # Save the processed data
+        np.save(output_file + '.npy', vecs)
+
+        return vecs
