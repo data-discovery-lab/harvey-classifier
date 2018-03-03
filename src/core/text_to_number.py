d7fd88fa267d5d6a53f1d46da041fed3f9f3b6bd
Hoang Long <litpuvn@gmail.com>
2018-03-03 12:47:09 -0600

    add create vectors for seq2seq

--- src/core/text_to_number.py
+++ src/core/text_to_number.py
@@ -1,4 +1,4 @@
-from ktext.preprocess import processor
+from core.preprocessor import processor
 import dill as dpickle
 import numpy as np
 
@@ -6,18 +6,17 @@ import numpy as np
 class TextToNumber:
 
     @staticmethod
-    def create_number_vector(raw_text_array, max_uniqie_words_size=8000, padding_maxlen=70, padding_position='post',
-                             output_file='nummerical_text'):
-        body_pp = processor(keep_n=max_uniqie_words_size, padding_maxlen=padding_maxlen, padding=padding_position)
+    def create_number_vector(raw_text_array, append_indicators=True, max_uniqie_words_size=8000, padding_maxlen=70, padding_position='post', output_file='nummerical_text'):
+        body_pp = processor(append_indicators=append_indicators, keep_n=max_uniqie_words_size, padding_maxlen=padding_maxlen, padding=padding_position)
         vecs = body_pp.fit_transform(raw_text_array)
 
         if output_file is not None:
             # Save the preprocessor
-            with open(output_file + '.dpkl', 'wb') as f:
+            with open(output_file + '_pp.dpkl', 'wb') as f:
                 dpickle.dump(body_pp, f)
 
             # Save the processed data
-            np.save(output_file + '.npy', vecs)
+            np.save(output_file + '_vecs.npy', vecs)
 
         return vecs
 
