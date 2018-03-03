f6a5d5de91fd084102b2ccfd9466c84b2cc7558b
Hoang Long <litpuvn@gmail.com>
2018-03-03 16:09:22 -0600

    support spliting train and test

--- src/util/text_file_spliter.py
+++ src/util/text_file_spliter.py
@@ -2,18 +2,19 @@
 import pandas as pd
 import argparse
 import logging
+from sklearn.model_selection import train_test_split
 
 def get_parser():
     """Get parser for command line arguments."""
     parser = argparse.ArgumentParser(description="Extract a part of file for testing purpose")
     parser.add_argument("-i",
-                        "--inputfile",
+                        "--input-file",
                         dest="input",
                         help="File name of the input csv file",
                         default=None)
 
     parser.add_argument("-o",
-                        "--outputfile",
+                        "--output-file",
                         dest="output",
                         help="Output csv file",
                         default="output.csv")
@@ -22,7 +23,13 @@ def get_parser():
                         "--count",
                         dest="count",
                         help="Line count to be extracted",
-                        default="2000")
+                        default=2000)
+
+    parser.add_argument("-t",
+                        "--test-size",
+                        dest="testSize",
+                        help="Percentage of test size",
+                        default=None)
 
     return parser
 
@@ -34,6 +41,7 @@ if __name__ == '__main__':
     fileInput = args.input
     fileOutput = args.output
     count = int(args.count)
+    testSize = args.testSize
 
     fp = pd.read_csv(fileInput, sep=',', header=None, chunksize=count)
 
@@ -41,8 +49,15 @@ if __name__ == '__main__':
         fileOutput = fileOutput + '.csv'
 
     for line in fp:
-        # f.write(line)  # python will convert \n to os.linesep
         line.to_csv(fileOutput, index=False, header=None)
+
+        if testSize is not None:
+            traindf, testdf = train_test_split(line, test_size=float(testSize))
+            traindf.to_csv('train.csv', index=False, header=None)
+            testdf.to_csv('test.csv', index=False, header=None)
+
         break
+
+
 
     logging.warning("Done")
