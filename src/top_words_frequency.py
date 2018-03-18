51e81ef438b7c6da6767eac15315f058f19ad76b
Hoang Long <litpuvn@gmail.com>
2018-03-18 13:28:55 -0500

    add stop words for dwdb

--- src/top_words_frequency.py
+++ src/top_words_frequency.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from core.tweet_reader import TweetReader
 
-reader = TweetReader('data/pos_neg/neg_comment.csv',  text_column='text', separator='\001', encoding='utf8')
+reader = TweetReader('/home/long/TTU-SOURCES/ttu-texas-water/data/tweets/twdb_monthly/2017-01.csv',  text_column='text', separator='\001', encoding='utf8')
 freq = reader.extract_words_frequency(num_words=10, stop_word_file='input/stopwords.txt', ordered='asc')
 
 words_names = []
