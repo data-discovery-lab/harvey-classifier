da7bf3b5fc5c5535948be85c06dc84a6ddfcf211
Hoang Long <litpuvn@gmail.com>
2018-03-25 12:52:15 -0500

    add median for topwords

--- src/top_words_frequency.py
+++ src/top_words_frequency.py
@@ -2,19 +2,56 @@ from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 from core.tweet_reader import TweetReader
+import statistics
+import operator
 
-reader = TweetReader('/home/long/TTU-SOURCES/ttu-texas-water/data/tweets/twdb_monthly/2017-02.csv',  text_column='text', separator='\001', encoding='utf8')
-wfreq = reader.extract_words_frequency(num_words=20, stop_word_file='input/stopwords.txt', ordered='asc')
+
+reader = TweetReader('data/need/full-day-need/09_02.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_24.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_25.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_26.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_27.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_28.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_29.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_30.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/08_31.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/09_01.csv',  text_column=1, separator='|', encoding='utf8')
+# reader.add_file_content_to_corpus('data/need/full-day-need/09_02.csv',  text_column=1, separator='|', encoding='utf8')
+
+wfreq = reader.extract_words_frequency(num_words=None, min_threshold=None, stop_word_file='input/harvey_stopwords.txt', ordered='asc')
+
+needs = TweetReader('/home/long/TTU-SOURCES/harvey-need/data/daily-need/needs.csv',  text_column=0, separator='|', encoding='utf8')
+need_corpus = needs.get_corpus()
 
 words_names = []
 words_count = []
+
+need_wfreq = dict()
+frequencies = []
+total_frequency = 0
+
 for (word, freq) in wfreq:
+    if word not in need_corpus:
+        continue
+    need_wfreq[word] = freq
+    total_frequency += freq
+    frequencies.append(freq)
+
+median_freq = statistics.median(frequencies)
+## remove low frequency items
+final_wfreq = dict()
+for word, freq in need_wfreq.items():
+    if freq > median_freq:
+        final_wfreq[word] = freq
+
+sorted_wfreq = sorted(final_wfreq.items(), key=operator.itemgetter(1))
+for word, freq in sorted_wfreq:
     words_names.append(word)
     words_count.append(freq)
 
-print(wfreq)
+print(final_wfreq)
 
-show_plot = False
+show_plot = True
 
 if show_plot == True:
     #
